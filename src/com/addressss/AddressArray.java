fcae9c1cc3f78cda8ca8ac9508b694507a52cd73
monali8370 <monalishinde952@gmail.com>
2023-12-18 14:04:29 +0530

    edit:existing contact use name

--- src/com/addressss/AddressArray.java
+++ src/com/addressss/AddressArray.java
@@ -1,34 +1,75 @@
-package com.addressss;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressArray {
+    static Scanner sc=new Scanner(System.in);
     ArrayList<AddressBook>obj=new ArrayList<>();
     public void addContact(){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first name: ");
         String firstName=sc.next();
         System.out.println("Enter last name: ");
-        String lastName=sc.next();
-        System.out.println("Enter address: ");
-        String address=sc.next();
-        System.out.println("Enter city: ");
-        String city=sc.next();
-        System.out.println("Enter state: ");
-        String state=sc.next();
-        System.out.println("Enter zip: ");
-        int zip=sc.nextInt();
-        System.out.println("Enter phone number: ");
-        int phone=sc.nextInt();
-        System.out.println("Enter email: ");
-        String email=sc.next();
-        AddressBook ab=new AddressBook(firstName,lastName,address,city,state,zip,phone,email);
-        AddressArray arr=new AddressArray();
-        arr.obj.add(ab);
-        System.out.println(ab);
+         public void addContact(){
+            System.out.println("Enter state: ");
+            String state=sc.next();
+            System.out.println("Enter zip: ");
+            int zip=sc.nextInt();
+            String zip=sc.next();
+            System.out.println("Enter phone number: ");
+            int phone=sc.nextInt();
+            String phone=sc.next();
+            System.out.println("Enter email: ");
+            String email=sc.next();
+            AddressBook ab=new AddressBook(firstName,lastName,address,city,state,zip,phone,email);
+            AddressArray arr=new AddressArray();
+            arr.obj.add(ab);
+            obj.add(ab);
+            System.out.println(ab);
+        }
+        public void editContact(){
+            System.out.println("Enter your name");
+            String name=sc.next();
+            for (AddressBook book:obj){
+                if (book.getFirstName().equalsIgnoreCase(name)){
+                    System.out.println("1. address\n2. city\n3. state\n4. zip\n5. phone\n6. email");
+                    System.out.println("select option");
+                    int option=sc.nextInt();
+                    switch (option){
+                        case 1:
+                            String address=sc.next();
+                            book.setAddress(address);
+                            break;
+                        case 2:
+                            String city=sc.next();
+                            book.setCity(city);
+                            break;
+                        case 3:
+                            String state=sc.next();
+                            book.setState(state);
+                            break;
+                        case 4:
+                            String zip=sc.next();
+                            book.setZip(zip);
+                            break;
+                        case 5:
+                            String phone=sc.next();
+                            book.setPhone(phone);
+                            break;
+                        case 6:
+                            String email=sc.next();
+                            book.setEmail(email);
+                            break;
+                        default:
+                            System.out.println("Select correct option!!!!");
+                            break;
+                    }
+                }
+            }
+            System.out.println(obj);
+        }
+        public static void main(String[] args) {
+            AddressArray arr=new AddressArray();
+            arr.addContact();
+            arr.editContact();
+        }
     }
-    public static void main(String[] args) {
-        AddressArray arr=new AddressArray();
-        arr.addContact();
-    }
-}
--- src/com/addressss/AddressBook.java
+++ src/com/addressss/AddressBook.java
@@ -9,7 +9,57 @@ public class AddressBook {
     private String state;
     private int zip;
     private int phone;
+    private String zip;
+    private String phone;
     private String email;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     @Override
     public String toString() {
         return "AddressBook: " +
@@ -23,17 +73,15 @@ public class AddressBook {
                 ", email='" + email + '\'';
     }
     public AddressBook(String firstName, String lastName, String address, String city, String state, int zip, int phone, String email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phone=phone;
-        this.email=email;
+    public AddressBook(String firstName, String lastName, String address, String city,
+                String state, String zip, String phone, String email){
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.address=address;
+            this.city=city;
+            this.state=state;
+            this.zip=zip;
+            this.phone=phone;
+            this.email=email;
+        }
     }
-    public static void main(String[] args) {
-        AddressBook add=new AddressBook("sd","adf","aef","aeef","wsbrv",2464,4634,"sger");
-        System.out.println(add);
-    }
-}
