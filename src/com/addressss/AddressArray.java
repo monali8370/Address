995782642495f7fb41efe2c524fcae24e4d1bb03
monali8370 <monalishinde952@gmail.com>
2023-12-18 14:09:46 +0530

    delete:person using person name

--- src/com/addressss/AddressArray.java
+++ src/com/addressss/AddressArray.java
@@ -2,61 +2,64 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressArray {
-    static Scanner sc=new Scanner(System.in);
-    ArrayList<AddressBook>obj=new ArrayList<>();
-    public void addContact(){
-        Scanner sc=new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+    ArrayList<AddressBook> obj = new ArrayList<>();
+
+    public void addContact() {
         System.out.println("Enter first name: ");
-        String firstName=sc.next();
+        String firstName = sc.next();
         System.out.println("Enter last name: ");
-         public void addContact(){
-            System.out.println("Enter state: ");
-            String state=sc.next();
-            System.out.println("Enter zip: ");
-            int zip=sc.nextInt();
-            String zip=sc.next();
-            System.out.println("Enter phone number: ");
-            int phone=sc.nextInt();
-            String phone=sc.next();
-            System.out.println("Enter email: ");
-            String email=sc.next();
-            AddressBook ab=new AddressBook(firstName,lastName,address,city,state,zip,phone,email);
-            AddressArray arr=new AddressArray();
-            arr.obj.add(ab);
-            obj.add(ab);
-            System.out.println(ab);
-        }
-        public void editContact(){
-            System.out.println("Enter your name");
-            String name=sc.next();
-            for (AddressBook book:obj){
-                if (book.getFirstName().equalsIgnoreCase(name)){
+        String lastName = sc.next();
+        System.out.println("Enter address: ");
+        String address = sc.next();
+        System.out.println("Enter city: ");
+        String city = sc.next();
+        System.out.println("Enter state: ");
+        String state = sc.next();
+        System.out.println("Enter zip: ");
+        String zip = sc.next();
+        System.out.println("Enter phone number: ");
+        String phone = sc.next();
+        System.out.println("Enter email: ");
+        String email = sc.next();
+        AddressBook ab = new AddressBook(firstName, lastName, address, city, state, zip, phone, email);
+        obj.add(ab);
+        System.out.println(ab);
+    }
+
+    public void editContact() {
+        System.out.println("Enter your name");
+        System.out.println("Enter the name for which the details to be edited: ");
+        String name = sc.next();
+        for (AddressBook book : obj) {
+            if (book.getFirstName().equalsIgnoreCase(name)) {
+                if (book.getFirstName().equals(name)) {
                     System.out.println("1. address\n2. city\n3. state\n4. zip\n5. phone\n6. email");
                     System.out.println("select option");
-                    int option=sc.nextInt();
-                    switch (option){
+                    int option = sc.nextInt();
+                    switch (option) {
                         case 1:
-                            String address=sc.next();
+                            String address = sc.next();
                             book.setAddress(address);
                             break;
                         case 2:
-                            String city=sc.next();
+                            String city = sc.next();
                             book.setCity(city);
                             break;
                         case 3:
-                            String state=sc.next();
+                            String state = sc.next();
                             book.setState(state);
                             break;
                         case 4:
-                            String zip=sc.next();
+                            String zip = sc.next();
                             book.setZip(zip);
                             break;
                         case 5:
-                            String phone=sc.next();
+                            String phone = sc.next();
                             book.setPhone(phone);
                             break;
                         case 6:
-                            String email=sc.next();
+                            String email = sc.next();
                             book.setEmail(email);
                             break;
                         default:
@@ -67,9 +70,23 @@ public class AddressArray {
             }
             System.out.println(obj);
         }
-        public static void main(String[] args) {
-            AddressArray arr=new AddressArray();
+        public void deleteContact () {
+            System.out.println("Enter the name by which the contact to be deleted: ");
+            String name = sc.next();
+            for (AddressBook book : obj) {
+                if (book.getFirstName().equals(name)) {
+                    obj.remove(book);
+                    break;
+                }
+            }
+            System.out.println(obj);
+        }
+        public static void main (String[]args){
+            AddressArray arr = new AddressArray();
+            arr.addContact();
             arr.addContact();
             arr.editContact();
+            arr.deleteContact();
         }
     }
+}
